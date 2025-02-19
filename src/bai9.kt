66ef650d67508cba4180fc939e824620969a729c
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2025-02-19 16:19:37 +0700

    update

--- src/bai9.kt
+++ src/bai9.kt
@@ -1,39 +1,43 @@
 import kotlin.random.Random
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    var playAgain: String
+    var choiTiep: String?
 
     do {
-        val targetNumber = Random.nextInt(0, 101)
-        var attempts = 0
-        var guessedCorrectly = false
+        val quest = Random.nextInt(0, 101)
+        var count = 0
+        var check = false
 
         println("Chào mừng đến với trò chơi Đoán số! Bạn có 5 lần thử.")
 
-        while (attempts < 5) {
-            print("Nhập một số từ 0-100: ")
-            val guess = scanner.nextInt()
-            attempts++
+        while (count < 5) {
+            var guess: String?
+            do {
+                print("Nhập một số từ 0-100: ")
+                guess = readLine()
+            }while (guess==null)
 
+            count++
             when {
-                guess == targetNumber -> {
-                    println("Chúc mừng! Bạn đã đoán đúng số $targetNumber.")
-                    guessedCorrectly = true
+                guess.toInt() == quest -> {
+                    println("Chúc mừng! Bạn đã đoán đúng số $quest.")
+                    check = true
                     break
                 }
-                guess < targetNumber -> println("Số bạn đoán nhỏ hơn số bí mật.")
+                guess.toInt() < quest -> println("Số bạn đoán nhỏ hơn số bí mật.")
                 else -> println("Số bạn đoán lớn hơn số bí mật.")
             }
         }
 
-        if (!guessedCorrectly) {
-            println("Bạn đã thua cuộc! Số đúng là $targetNumber.")
+        if (!check) {
+            println("Bạn đã thua cuộc! Số đúng là $quest.")
         }
 
-        print("Bạn có muốn chơi lại không? (y/n): ")
-        playAgain = scanner.next().lowercase()
-    } while (playAgain == "y")
+        do {
+            print("Bạn có muốn chơi lại không? (y/n): ")
+            choiTiep = readLine()?.lowercase()
+        }while (choiTiep==null)
+    } while (choiTiep == "y")
 
     println("Kết thúc chương trình.")
 }
